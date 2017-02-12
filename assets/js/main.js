18e5ac990b0aadf1122295245488763977c7f2ae
ntmedina <nataliamedina.rj@gmail.com>
2017-02-12 21:37:13 -0200

    Fixing menu interaction 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 	$('.menu-ic').on('click', function(){
 		$('header').toggleClass('header-nav'); 
-		$('.menu').toggleClass('menu-nav');     
-		$('.social-link').toggleClass('social-nav');       
+		$('.menu').toggleClass('menu-nav');       
 	})
 }) 
