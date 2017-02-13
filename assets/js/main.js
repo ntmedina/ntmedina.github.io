6a59a6360ff1cd07c19a7fa5ba3fd59a031d40f5
ntmedina <nataliamedina.rj@gmail.com>
2017-02-13 01:34:48 -0200

    Adding close icon to menu interaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
 	$('.menu-ic').on('click', function(){
 		$('header').toggleClass('header-nav'); 
-		$('.menu').toggleClass('menu-nav');       
+		$('.menu').toggleClass('menu-nav');   
+		$('.close-ic').toggleClass('close-ic-nav');  
+		$('.menu-ic').toggleClass('menu-ic-nav');    
 	})
+	$('.close-ic').on('click', function(){
+		$('header-nav').addClass('header')
+		$('header').removeClass('header-nav');
+
+		$('.menu-ic-nav').addClass('menu-ic');
+		$('.menu-ic').removeClass('menu-ic-nav');
+
+		$('.menu-nav').addClass('menu');
+		$('.menu').removeClass('menu-nav');
+
+		$('header-nav').addClass('.header');
+		$('header').removeClass('header-nav');
+
+		$('.close-ic-nav').addClass('close-ic')
+		$('.close-ic').removeClass('close-ic-nav');   
+	})
+
 }) 
